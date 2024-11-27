452cb0bece70a9baf956faff6b9e603fa006b726
rinitsi <banitsirachel@gmailcom>
2024-11-27 09:28:26 +0000

    adding all files

---
+++ src folder/index.js
@@ -0,0 +1,6 @@
+alert("Hello Welcome to our Home Page")
+
+let name = prompt("What is your name")
+let age = prompt("What is your age")
+let email = prompt("What is your email adress")
+alert(" Hi " +  name  + " you are " +  age  + " years old and we will keep in touch via your email: "  +  email )
